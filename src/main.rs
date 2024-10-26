71ceee9b334c2d5e1bf31feee57dd813d08e88f2
Mincheo <dasalks2@gmail.com>
2024-10-26 15:25:06 +0800

    fix: change path params to argument params

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use axum::extract::Query;
+use axum::response::Html;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Name {
+    name: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/hello/:name", get(hello_handler));
+    let app = Router::new().route("/hello", get(hello_handler));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
     println!("Listen on {}", addr);
@@ -14,6 +22,7 @@ async fn main() {
     .unwrap();
 }
 
-async fn hello_handler(axum::extract::Path(name): axum::extract::Path<String>) -> String {
-    format!("Hello, {}", name)
+async fn hello_handler(Query(params): Query<Name>) -> Html<String> {
+    let html_content = format!("Hello, {}", params.name);
+    Html(html_content)
 }
